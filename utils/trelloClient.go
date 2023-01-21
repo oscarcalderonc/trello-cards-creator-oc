75a1249a800ff1c96cf51b1f9d32d0ed4af8df3f
Oscar Calderon <o@oscarcalderon.com>
2023-01-20 19:02:45 -0600

    feat(trello): implementing api methods to call trello

--- utils/trelloClient.go
+++ utils/trelloClient.go
@@ -1,10 +1,21 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/go-faker/faker/v4"
+	"io"
+	"math/rand"
+	"net/http"
 	"trello-cards-creator-oc/models"
 )
+
+type TrelloMember struct {
+	Id       string `json:"id"`
+	FullName string `json:"fullName"`
+	Username string `json:"username"`
+}
 
 type TrelloClient struct {
 	trelloApi  *models.TrelloApi
@@ -35,27 +46,34 @@ type TrelloCard struct {
 
 func (tc *TrelloClient) CreateIssueCard(issue *models.IssueCard) error {
 	trelloCard := TrelloCard{
-		Name:     issue.Title,
-		List:     tc.trelloIds.TrelloIssuesListId,
-		Position: cardPosition,
+		Name:        issue.Title,
+		Description: issue.Description,
+		List:        tc.trelloIds.TrelloIssuesListId,
+		Position:    cardPosition,
 	}
-	fmt.Println(trelloCard)
-	return nil
+
+	return tc.createTrelloCard(&trelloCard)
 }
 
 func (tc *TrelloClient) CreateBugCard(bug *models.BugCard) error {
 	cardName := fmt.Sprintf("bug-%s-%v", faker.Word(), faker.CCNumber())
+
+	members, _ := tc.getBoardMembers()
+
+	randomMemberIdx := rand.Intn(len(members))
+	if randomMemberIdx > 0 {
+		randomMemberIdx--
+	}
 
 	trelloCard := TrelloCard{
 		Name:        cardName,
 		Description: bug.Description,
 		List:        tc.trelloIds.TrelloGeneralListId,
 		Position:    cardPosition,
+		Members:     []string{members[randomMemberIdx].Id},
 		Labels:      []string{tc.trelloIds.TrelloBugLabelId},
 	}
-
-	fmt.Println(trelloCard)
-	return nil
+	return tc.createTrelloCard(&trelloCard)
 }
 
 func (tc *TrelloClient) CreateTaskCard(task *models.TaskCard) error {
@@ -77,6 +95,34 @@ func (tc *TrelloClient) CreateTaskCard(task *models.TaskCard) error {
 		Labels:   []string{categoryLabel},
 	}
 
-	fmt.Println(trelloCard)
-	return nil
+	return tc.createTrelloCard(&trelloCard)
+}
+
+func (tc *TrelloClient) createTrelloCard(card *TrelloCard) error {
+	payload, _ := json.Marshal(card)
+
+	url := fmt.Sprintf("%s/1/cards?%s", tc.baseUrl, tc.authParams)
+
+	res, err := http.Post(url, "application/json", bytes.NewReader(payload))
+	fmt.Println(res.Body)
+
+	return err
+}
+
+func (tc *TrelloClient) getBoardMembers() ([]TrelloMember, error) {
+
+	url := fmt.Sprintf("%s/1/boards/%s/members?%s", tc.baseUrl, tc.trelloIds.TrelloBoardId, tc.authParams)
+
+	res, err := http.Get(url)
+
+	fmt.Println(res.Body)
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+
+	var members []TrelloMember
+
+	err = json.Unmarshal(body, &members)
+
+	return members, err
 }
